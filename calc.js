3994b7d7465cc101f8497da1e465c3e0bf8f2295
yangmeijing <1806102644@qq.com>
2019-04-02 14:09:14 +0800

    矩形计算器合法性校验

--- calc.js
+++ calc.js
@@ -12,5 +12,28 @@ function Rectangle(width, height) {
     return 2 * (w + h);
             
   };
-
 }
+/* exported valid */
+function valid(data){
+  var result = {
+    idOK:false,
+    reason:''
+  };
+  if(data===''){
+    result.reason='不能为空！';
+    return result;
+  }
+  if(Number(data)<0){
+    result.reason='不能小于0！';
+    return result;
+  }
+  if(!/^-?(0|[1-9]\d*)(\.\d*)?([eE][+-]?\d+)?$/.test(data)){
+    result.reason='不是合法数字！';
+    return result;
+  }
+  result.reason=data; 
+  result.isOK='true';
+  return result;
+}
+
+
--- rectangle.js
+++ rectangle.js
@@ -1,14 +1,48 @@
-/* global Rectangle: true */
+/* global Rectangle,valid: true */
 $(function(){
   var $width=$('#width'),
       $height=$('#length'),
       $btn=$('#btn'),
       $per=$('#perimeter'),
-      $area=$('#area');
-
+      $area=$('#area'),
+      $widthValidation=$('#width-validation'),
+      $heightValidation=$('#length-validation');
+  $width.focusout(function(){
+    var result = valid($width.val());
+    console.log($width.val());
+    console.log(result);
+    if(!result.isOK){
+      $widthValidation.html('宽度'+result.reason);
+    }else{
+      $widthValidation.html('');
+    }
+  });
+  $height.focusout(function(){
+    console.log($height.val());
+    var result = valid($height.val());
+    if(!result.isOK){
+      $heightValidation.html('长度'+result.reason);
+    }else{
+      $heightValidation.html('');
+    }
+  });
+ 
   $btn.click(function(){
     var w = $width.val(),
         h = $height.val();        
+    var result = valid($width.val());
+    var validWidth = valid(w);
+    var validHeight = valid(h);
+
+
+    if(!validWidth.isOK){
+      $widthValidation.html('长度'+result.reason);
+      return;
+    }
+    if(!validHeight.isOK){
+      $heightValidation.html('宽度'+result.reason);
+      return;
+    }
 
     var r = new Rectangle(w, h);
             
